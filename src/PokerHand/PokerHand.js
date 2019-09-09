af5b35f97c74cbb3e3778906aba41a0df3d028b2
Davis, Jared <jared.davis@transamerica.com>
2019-09-08 20:41:48 -0600

    worked on poker hand verfication.  Built out methods to handle poker hands highlighted in documentation

--- src/PokerHand/PokerHand.js
+++ src/PokerHand/PokerHand.js
@@ -4,12 +4,140 @@
 * Please refer to the README.md for challenge questions and complete your challenge below.
 */
 export default class PokerHand {
+  
   constructor(hand) {
     this.hand = hand;
   }
 
+  /**
+   * @method getRank
+   *
+   * handles current string of pocker hand variable, sanitizes string to consumable object
+   * checks stanized string values to determine why type of poker hand was passed to it 
+   *
+   * * @return {String} return string value of pocker hand 
+   */
   getRank = () => {
-    // TODO: Implement poker hand ranking
-    return 'Royal Flush';
+    const rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const sanitisedCards = this.santizeCards(this.hand);
+    const rankedCards = this.rankCards(sanitisedCards, rank);
+    const isStraight = this.isStraight(rankedCards, rank);
+    const isFlush = this.isFlush(sanitisedCards, rank);
+  
+    switch(true) {
+      case isStraight && isFlush && rankedCards[0][0].type == 'A':
+        return 'Royal Flush';
+      case isStraight && isFlush: 
+        return 'Straight Flush';
+      case isFlush: 
+        return 'Flush';
+      case isStraight: 
+      return 'Straight';
+      case rankedCards[0].length === 3 && rankedCards[1].length === 2:
+        return 'Full House';
+      case rankedCards[0].length === 4:
+        return 'Four of a Kind';
+      case rankedCards[0].length === 3:
+        return 'Three of a Kind';
+      case rankedCards[0].length === 2 && rankedCards[1].length === 2:
+        return 'Two Pair';
+      case rankedCards[0].length === 2:
+        return 'One Pair';
+      default:
+          return 'High Card';
+    }
   };
+
+ /**
+   * @method santizeCards
+   *
+   * handles string value of current pocker hand, creates array from string value
+   * maps over newly created array and creates array card objects
+   * 
+   * @param {string} string value of current card hand 
+   * @return {cardObj[]} returns an array of card objects 
+  */
+  santizeCards = (cards) => {
+    const cardsArray = cards.split(" ");
+    const sanitizedCards = cardsArray.map((card) => {
+      let cardObj = {};
+      cardObj = {
+        type: card.substring(0, card.length - 1),
+        suit: card.slice(-1),
+      };
+      return cardObj;
+    })
+    return sanitizedCards;
+  }
+
+  /**
+   * @method rankCards
+   *
+   * takes array of cardObjects and creates a new array of cardObjects order in rank of their type 
+   * new array of card objects with be returned in an order of highest to lowest in ranking
+   * 
+   * @param {cardObj[], string[]} array of sanitized card objects and card rankings
+   * @return {cardObj[]} returns an array of card objects 
+  */
+  rankCards = (cards, cardRankings) => {
+    let rankArray = [];
+
+    cards.map((card) => {
+      let cardRank = cardRankings.indexOf(card.type);
+      rankArray[cardRank] = rankArray[cardRank] || [];
+      rankArray[cardRank].push(card);
+    })
+
+    rankArray = rankArray.filter((cardRank) =>  (cardRank === 0) ? false : true);
+    rankArray.reverse();
+    rankArray.sort((rankArray1, rankArray2) => {
+      return rankArray1.length > rankArray2.length ? -1 : rankArray1.length < rankArray2.length ? 1 : 0;
+    });
+    return rankArray;
+  }
+
+  /**
+   * @method isFlush
+   *
+   * checks to determine if array of card objects all have the same type of suit
+   * 
+   * @param {cardObj[]} array of sanitized card objects
+   * @return {booelan} returns whether array of cards meet criteria of straight 
+  */
+  isFlush = (cards) => {
+    const cardCompareSuit = cards[0].suit;
+
+    for (let card of cards) {
+      if (card.suit != cardCompareSuit) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * @method isStraight
+   *
+   * checks to determine if array of card objects to see if card types are in numerical order
+   * check for "wheel" with Ace being low card as well
+   * 
+   * @param {cardObj[], string[]} array of sanitized card objects and card rankings
+   * @return {boolean} returns whether array of cards meet criteria of straight 
+  */
+  isStraight = (cards, cardRanks) => {
+    const cardLength = 4
+    if (!cards[cardLength]) {
+      return false;
+    }
+
+    if (cards[0][0].type === 'A' && cards[1][0].type === '5' && cards[4][0].type === '2') {
+      return true;
+    }
+
+    
+    let firstRankedCard = cardRanks.indexOf(cards[0][0].type);
+    let lastRankedCard = cardRanks.indexOf(cards[4][0].type);
+    return (firstRankedCard - lastRankedCard) === cardLength;
+  }
 }
--- test/PokerHand/pokerHand.test.js
+++ test/PokerHand/pokerHand.test.js
@@ -3,11 +3,30 @@ const assert = require('chai').assert;
 import PokerHand from '../../src/PokerHand/PokerHand';
 
 describe('PokerHand', () => {
+
   it('can rank a royal flush', () => {
-    const hand = new PokerHand('As Ks Qs Js 10s');
+    const hand = new PokerHand('10s Js Ks Qs As');
     assert.deepEqual(hand.getRank(), 'Royal Flush');
   });
 
+  it('can rank a straight flush', () => {
+    const hand = new PokerHand('2s 3s 4s 5s 6s');
+    assert.deepEqual(hand.getRank(), 'Straight Flush');
+  });
+
+  it('can rank a straight', () => {
+    const hand = new PokerHand('2s 3f 4s 5h As');
+    assert.deepEqual(hand.getRank(), 'Straight');
+  });
+
+  it('can rank a flush', () => {
+    const hand = new PokerHand('2s 3s 4s 5s 9s');
+    assert.deepEqual(hand.getRank(), 'Flush');
+  });
+  it('can rank a high card', () => {
+    const hand = new PokerHand('Ah Qs 10c 7d 2s');
+    assert.deepEqual(hand.getRank(), 'High Card');
+  });
   it('can rank a pair', () => {
     const hand = new PokerHand('Ah As 10c 7d 6s');
     assert.deepEqual(hand.getRank(), 'One Pair');
@@ -18,10 +37,19 @@ describe('PokerHand', () => {
     assert.deepEqual('Two Pair', hand.getRank());
   });
 
-  it('can rank a flush', () => {
-    const hand = new PokerHand('Kh Qh 6h 2h 9h');
-    assert.deepEqual('Flush', hand.getRank());
+  it('can rank three of a kind', () => {
+    const hand = new PokerHand('Kh Kc Ks 3h 2d');
+    assert.deepEqual('Three of a Kind', hand.getRank());
   });
 
+  it('can rank four of a kind', () => {
+    const hand = new PokerHand('Kh Kc Ks Kh 2d');
+    assert.deepEqual('Four of a Kind', hand.getRank());
+  });
+
+  it('can rank a full house', () => {
+    const hand = new PokerHand('Kh Kc Ks Ah Ad');
+    assert.deepEqual('Full House', hand.getRank());
+  });
   // TODO: More tests go here
 });
